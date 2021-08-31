65b55d15d3beb90590bfcc0346ef1f6c57278c86
JerryYouxin <youxin2015@outlook.com>
2021-09-01 00:12:48 +0800

    update API for vlogger and vreporter

--- src/clients/value_freq/value_freq.cpp
+++ src/clients/value_freq/value_freq.cpp
@@ -1,43 +1,67 @@
 #include "dr_api.h"
 #include "vprofile.h"
+
 vtrace_t* vtrace;
+vreporter_t* vreport;
 
-VLogger::MetricLogs<uint64_t, uint64_t> int_freq(true);
-VLogger::MetricLogs<uint32_t, uint64_t> sp_freq(true);
-VLogger::MetricLogs<uint64_t, uint64_t> dp_freq(true);
+logger_t int_freq, sp_freq, dp_freq;
 
-static void
-PrintTopN() {
+void int_handler((vreporter_t* vreporter, uint64_t& key, uint64_t& data, uint64_t& global) {
+    vreporter->metric("Value", key);
+    vreporter->metric("Frequency", data, global);
+}
 
+void sp_handler((vreporter_t* vreporter, uint32_t& key, uint64_t& data, uint64_t& global) {
+    vreporter->metric("Value", *reinterpret_cast<float*>(&key));
+    vreporter->metric("Frequency", data, global);
+}
+
+void dp_handler((vreporter_t* vreporter, uint64_t& key, uint64_t& data, uint64_t& global) {
+    vreporter->metric("Value", *reinterpret_cast<double*>(&key));
+    vreporter->metric("Frequency", data, global);
 }
 
 static void
 ClientInit(int argc, const char *argv[])
 {
+    vlogger_init();
+    int_freq = VLogger::register_logger<uint64_t, uint64_t>();
+    sp_freq = VLogger::register_logger<uint32_t, uint64_t>();
+    dp_freq = VLogger::register_logger<uint64_t, uint64_t>();
+    vreporter_init("value_freq", VREPORTER_DEFAULT);
+    vreport = vreporter_create("", TEXT_REPORT);
+    vreporter_register_section(vreport, "INT", int_freq, int_handler, true, 100, true);
+    vreporter_register_section(vreport, "SINGLE", sp_freq, sp_handler, true, 100, true);
+    vreporter_register_section(vreport, "DOUBLE", dp_freq, dp_handler, true, 100, true);
 }
 
 static void
 ClientExit(void)
 {
-    PrintTopN();
+    vreporter_destroy(vreport);
+    vreporter_exit();
     vprofile_unregister_trace(vtrace);
     vprofile_exit();
+    VLogger::unregister_logger(int_freq);
+    VLogger::unregister_logger(sp_freq);
+    VLogger::unregister_logger(dp_freq);
     vlogger_exit();
 }
 
 template<int size, int esize, bool is_float>
 void update(val_info_t *info) {
+    void* logger_tls = VLogger::getLoggerTLS();
     const int num = size/esize;
     if(is_float) {
         switch(esize) {
             case 4:
                 for(int i=0; i<num; ++i) {
-                    sp_freq.increament(reinterpret_cast<uint32_t*>(info->val)[i]);
+                    VLogger::increament<uint64_t>(logger_tls, sp_freq, reinterpret_cast<uint32_t*>(info->val)[i]);
                 }
                 break;
             case 8:
                 for(int i=0; i<num; ++i) {
-                    dp_freq.increament(reinterpret_cast<uint64_t*>(info->val)[i]);
+                    VLogger::increament<uint64_t>(logger_tls, dp_freq, reinterpret_cast<uint64_t*>(info->val)[i]);
                 }
                 break;
             default:
@@ -47,22 +71,22 @@ void update(val_info_t *info) {
         switch(esize) {
             case 1:
                 for(int i=0; i<num; ++i) {
-                    int_freq.increament(reinterpret_cast<uint8_t*>(info->val)[i]);
+                    VLogger::increament<uint64_t>(logger_tls, int_freq, reinterpret_cast<uint8_t*>(info->val)[i]);
                 }
                 break;
             case 2:
                 for(int i=0; i<num; ++i) {
-                    int_freq.increament(reinterpret_cast<uint16_t*>(info->val)[i]);
+                    VLogger::increament<uint64_t>(logger_tls, int_freq, reinterpret_cast<uint16_t*>(info->val)[i]);
                 }
                 break;
             case 4:
                 for(int i=0; i<num; ++i) {
-                    int_freq.increament(reinterpret_cast<uint32_t*>(info->val)[i]);
+                    VLogger::increament<uint64_t>(logger_tls, int_freq, reinterpret_cast<uint32_t*>(info->val)[i]);
                 }
                 break;
             case 8:
                 for(int i=0; i<num; ++i) {
-                    int_freq.increament(reinterpret_cast<uint64_t*>(info->val)[i]);
+                    VLogger::increament<uint64_t>(logger_tls, int_freq, reinterpret_cast<uint64_t*>(info->val)[i]);
                 }
                 break;
             default:
@@ -81,7 +105,6 @@ dr_client_main(client_id_t id, int argc, const char *argv[])
     dr_set_client_name("DynamoRIO Client 'value_freq'",
                        "http://dynamorio.org/issues");
     ClientInit(argc, argv);
-    vlogger_init();
     vprofile_init(VPROFILE_FILTER_ALL_INSTR, NULL, NULL,
                      VPROFILE_DEFAULT);
     vtrace = vprofile_allocate_trace(false, false, false, false, false);
--- src/vprofile/vlogger/vlogger.h
+++ src/vprofile/vlogger/vlogger.h
@@ -2,19 +2,26 @@
 #define __VLOGGER_H__
 
 /* Low-level VLogger Interfaces for VProfile Framework */
+
+typedef int logger_t;
+
 namespace VLogger {
     template<typename key_t, typename data_t>
-    class MetricLogs {
-        public:
-        MetricLogs();
-        MetricLogs(bool thread_merge);
+    logger_t register_logger();
+    void unregister_logger(int logger);
 
-        void increament(key_t key);
+    void* getLoggerTLS();
 
-        private:
+    // logger_tls->logger[key] += data
+    template<typename key_t, typename data_t>
+    void accumulate(void* logger_tls, logger_t logger, key_t& key, data_t& data);
 
-    };
+    // logger_tls->logger[key]++
+    template<typename key_t>
+    void increament(void* logger_tls, logger_t logger, key_t& key);
 }
+
+
 
 bool vlogger_init(void);
 void vlogger_exit(void);
